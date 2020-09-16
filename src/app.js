d7b91f8112f525aff43eda953a954e68caab26bc
andrew dillon <majik895@gmail.com>
2020-09-16 17:48:39 -0400

    navbar started

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { JumbotronContainer } from './containers/jumbotron'
-import {FooterContainer} from './containers/footer'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <>
-    <JumbotronContainer />
-    <FooterContainer />
-    </>
-  )
+    <Router>
+        <Route exact path="/browse">
+        <Browse />
+      </Route>
+        <Route exact path="/signin">
+        <SignIn />
+      </Route>
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+        </Route>
+    </Router>
+  );
 }
-
