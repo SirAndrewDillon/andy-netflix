1e86d626da4ccc51ff0df38b583438bb2ee898f3
andrew dillon <majik895@gmail.com>
2020-09-16 15:16:49 -0400

    footer working

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -22,8 +22,8 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-  grid-gap: 15px;
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
+  grid-gap: 25px;
   @media (max-width: 1000px) {
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
